034ac29ef311df2f8493fd33079ae61c26aa5937
victorvijay1210 <vijayvj1210@gmail.com>
2025-01-07 07:40:34 +0530

    pbs added

---
+++ src/CoreConcepts/Strings/StringManipulations.java
@@ -0,0 +1,43 @@
+package CoreConcepts.Strings;
+
+public class StringManipulations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		// String immutable
+//		String str1 = "Java";
+//		str1.concat("Core");
+//		System.out.println(str1);
+//		// The result should be assigned to a new reference variable (or same
+//		// variable) can be reused.
+//		String concat = str1.concat("Core");
+//		System.out.println(concat); // value1value2
+//
+//		// String methods
+//		String str2 = "abcdefghijk";
+//		// charAt(int paramInt)
+//		System.out.println(str2.charAt(2)); // prints a char - c
+//
+//		// concat(String paramString)
+//		System.out.println(str2.concat("lmn"));// abcdefghijklmn
+//
+//		System.out.println("ABC".equalsIgnoreCase("abc"));// true
+//		System.out.println("ABCDEFGH".length());// 8
+//
+//		// replace(char paramChar1, char paramChar2)
+//		System.out.println("012301230123".replace('0', '4'));// 412341234123
+//
+//		// replace(CharSequence paramCharSequence1, CharSequence
+//		// paramCharSequence2)
+//		System.out.println("012301230123".replace("01", "45"));// 452345234523
+		
+		// All characters from index paramInt
+				// String substring(int paramInt)
+				System.out.println("abcdefghij".substring(3)); // defghij
+				// 0123456789
+
+
+	}
+
+}
--- src/CoreConcepts/Strings/SubStringExample.java
+++ src/CoreConcepts/Strings/SubStringExample.java
@@ -2,6 +2,7 @@ package CoreConcepts.Strings;
 
 public class SubStringExample {
 	
+	//checks substring is available
 	public static boolean isSubString(String s, String sub) {
 		
 		if(s.contains(sub)){
@@ -11,11 +12,26 @@ public class SubStringExample {
 		return false;
 		
 	}
+	
+	//count the substring occurance
+	public static int countOccurance(String s, String sub) {
+		
+		int count = 0;
+		int index =0;
+		
+		while((index= s.indexOf(sub, index))!=-1) {
+			count ++;
+			index+=sub.length();
+		}
+		
+		return count;
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(isSubString("Hello World", "Worlds"));
-
+        System.out.println(countOccurance("Hello, World! World!", "World"));
 	}
 
 }
