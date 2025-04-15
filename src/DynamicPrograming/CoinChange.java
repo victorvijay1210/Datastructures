da8b3e1d528f94b69bd1edc05d02b15001e8e6ff
victorvijay1210 <70469583+victorvijay1210@users.noreply.github.com>
2025-04-15 07:20:25 +0530

    dp programing

---
+++ src/DynamicPrograming/CoinChange.java
@@ -0,0 +1,34 @@
+package DynamicPrograming;
+
+public class CoinChange {
+
+	public static int coinChange(int[] coins, int amount) {
+
+	    // Check edge case
+	    if (amount < 1) return 0;
+
+	    // Create DP array
+	    int[] minCoinsDP = new int[amount + 1];
+
+	    for (int i = 1; i <= amount; i++) {
+
+	      minCoinsDP[i] = Integer.MAX_VALUE;
+
+	      // Try each coin
+	      for (int coin : coins) {
+	        if (coin <= i && minCoinsDP[i - coin] != Integer.MAX_VALUE)
+	          minCoinsDP[i] = Math.min(minCoinsDP[i], 1 + minCoinsDP[i - coin]);
+	      }
+	    }
+
+	    return minCoinsDP[amount] == Integer.MAX_VALUE ? -1 : minCoinsDP[amount];
+	  }
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println(coinChange(new int[] {1,2,5}, 11));
+	}
+
+}
---
+++ src/DynamicPrograming/WordBreak.java
@@ -0,0 +1,53 @@
+package DynamicPrograming;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+//https://leetcode.com/problems/word-break/description/?envType=study-plan-v2&envId=top-interview-150
+public class WordBreak {
+    
+	//Time Complexity O(n^2)
+	//Space Complexity O(n)
+	public static boolean wordBreak(String s, List<String> wordlist) {
+
+		// Convert the dictionary to a set for O(1) lookups
+		Set<String> words = new HashSet<>(wordlist);
+
+		// Find the maximum word length in the dictionary
+		int maxWordLength = 0;
+		for (String word : wordlist) {
+			maxWordLength = Math.max(maxWordLength, word.length());
+		}
+
+		int n = s.length();
+
+		boolean[] dp = new boolean[n + 1];
+
+		// Base case: empty string is valid
+		dp[0] = true;
+
+		// Check prefixes of length up to maxLen
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = i - 1; j >= Math.max(0, i - maxWordLength); j--) {
+
+				if (dp[j] & words.contains(s.substring(j, i))) {
+
+					dp[i] = true;
+					break;
+				}
+			}
+
+		}
+
+		return dp[n];
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(wordBreak("leetcode", List.of("leet", "code"))); // true
+		System.out.println(wordBreak("applepenapple", List.of("apple", "pen")));// true
+		System.out.println(wordBreak("catsandog", List.of("cats", "dog", "sand", "and", "cat")));// false
+
+	}
+}
