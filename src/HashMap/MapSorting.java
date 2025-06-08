2ada157f122a9c1f2c37dbbdaf0045216cb36331
victorvijay1210 <70469583+victorvijay1210@users.noreply.github.com>
2025-06-08 08:22:31 +0530

    map sorting

---
+++ src/HashMap/MapSorting.java
@@ -0,0 +1,77 @@
+package HashMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class MapSorting {
+
+	
+	public static void sortDescByNumber(String[] str, int[] nums){
+	 
+		  Map<String, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<str.length;i++) {
+	
+			map.put(str[i], nums[i]);
+		}
+		
+		 List<Map.Entry<String,Integer>> list = new ArrayList<>(map.entrySet());
+		 
+          list.sort((a,b)->{
+        	  return b.getValue()-a.getValue();
+          }   
+        		  );
+          
+          
+       for(Map.Entry<String, Integer> data:list)	{
+    	 
+    	   System.out.println(data.getKey()+"="+data.getValue());
+     }
+		
+	}
+	
+	public static void sortAsecByNumber(String[] str, int[] nums){
+		 
+		  Map<String, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<str.length;i++) {
+	
+			map.put(str[i], nums[i]);
+		}
+		
+		
+		List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String,Integer>>(map.entrySet());
+		
+		list.sort((a,b)->{
+			return a.getValue()-b.getValue();
+		}
+				);
+		 
+       
+        
+        
+     for(Map.Entry<String, Integer> data:list)	{
+  	 
+  	   System.out.println(data.getKey()+"="+data.getValue());
+   }
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		String[] str = {"Alice", "Bob", "Charlie"};
+		int[] nums= {88,75,93};
+		
+	   sortDescByNumber(str, nums);
+	  sortAsecByNumber(str, nums);
+		
+	}
+
+}
